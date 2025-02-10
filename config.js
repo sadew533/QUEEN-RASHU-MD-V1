83e6cec7f35ee11b8850b7c260e42e3765e322ef
Malaka md <sadewnethmindya@gmail.com>
2025-02-10 08:36:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Past Your Session Id 🍃",
+SESSION_ID: process.env.SESSION_ID || "𝐐𝐔𝐄𝐄𝐍-𝐑𝐀𝐒𝐇𝐔-𝐌𝐃=FopGQBiR#EKX7rPXRDgxi-V7XcoEyTqGUX60THDSLkm2BI46kJGA",
 //උඩ ☝️ තියෙන Past Sesion Id කියන එක අයින් කරලා ඔයාගෙ Sesion Id එක Past කරන්න 😁
-MONGODB: process.env.MONGODB || "Past Your MongoDB Url 🍃",
+MONGODB: process.env.MONGODB || "",
 //උඩ ☝️ තියෙන Past MongoDB Url කියන එක අයින් කරලා ඔයාගෙ MongoDB Url එක Past කරන්න 😁
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
 BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
